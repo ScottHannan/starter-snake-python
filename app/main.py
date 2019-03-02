e793113945316e313f4157641ecad2bb2655f2d4
Scott Hannan <scott.n.hannan@gmail.com>
2019-03-02 15:13:46 -0800

    THANOS

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ def ping():
 def start():
 	data = bottle.request.json
 	
-	
+	print(data)
 	global target_x
 	global target_y
 	target_x = data["board"]["food"][0]["x"]
@@ -50,7 +50,7 @@ def start():
 	"""
 	#print(json.dumps(data))
 
-	color = "#00FF00"
+	color = "#000000"
 
 	return start_response(color)
 
@@ -62,6 +62,10 @@ def move():
 	global target_y
 	global behaviour
 	data = bottle.request.json
+	
+	if(data["turn"] == 3):
+		behaviour = "chase tail"
+	
 	
 	
 	body = data["you"]["body"]	# list of dictionaries
@@ -84,9 +88,14 @@ def move():
 	directions = ['up', 'down', 'left', 'right']
 	direction = 'none'
 	
+	if(health < 80):
+		behaviour = "food"
+	else:
+		behaviour = "chase tail"
+	
 	if(head_x == target_x and head_y == target_y):
-		target_x = food[0]["x"]
-		target_y = food[0]["y"]
+		target_x = food[random.randint(0, len(food))]["x"]
+		target_y = food[random.randint(0, len(food))]["y"]
 	
 	if(head_x == 0):
 		directions.remove("left")
@@ -112,6 +121,22 @@ def move():
 	
 	#directions is now only movements that won't kill the snake from walls or itself
 	
+	for i in range(0, len(other_snakes)):
+		for j in range(0, len(other_snakes[i]["body"])):
+			part_x = other_snakes[i]["body"][j]["x"]
+			part_y = other_snakes[i]["body"][j]["y"]
+			if(head_x - part_x == 1 and head_y - part_y == 0 and "left" in directions):
+				directions.remove("left")
+			if(head_x - part_x == -1 and head_y - part_y == 0 and "right" in directions):
+				directions.remove("right")
+			if(head_y - part_y == 1 and head_x - part_x == 0 and "up" in directions):
+				directions.remove("up")
+			if(head_y - part_y == -1 and head_x - part_x == 0 and "down" in directions):
+				directions.remove("down")
+				
+	#directions now only movements that won't kill it from running into another snake
+	
+		
 	#seek food
 	if(behaviour == "food"):
 		if(head_x - target_x > 0 and "left" in directions):
@@ -122,8 +147,35 @@ def move():
 			direction = 'up'
 		elif(head_y - target_y < 0 and "down" in directions):
 			direction = 'down'
-	elif(behavour == "chase tail"):
-		pass
+	elif(behaviour == "chase tail"):
+		if(head_x - body[-1]["x"] > 0 and "left" in directions):
+			direction = 'left'
+		elif(head_x - body[-1]["x"] < 0 and "right" in directions):
+			direction = 'right'
+		if(head_y - body[-1]["y"] > 0 and "up" in directions):
+			direction = 'up'
+		elif(head_y - body[-1]["y"] < 0 and "down" in directions):
+			direction = 'down'
+			
+	#check if moving in chosen direction will result in a head on collision
+	for i in range(0, len(other_snakes)):
+		if(direction == "left"):
+			if(other_snakes[i]["body"][0]["x"] == head_x - 2):
+				if(len(other_snakes[i]["body"]) >= len(body)):
+					direction = directions[(directions.index(direction)+1) % len(directions)]
+				
+		elif(direction == "right"):
+			if(other_snakes[i]["body"][0]["x"] == head_x + 2):
+				if(len(other_snakes[i]["body"]) >= len(body)):
+					direction = directions[(directions.index(direction)+1) % len(directions)]
+		elif(direction == "up"):
+			if(other_snakes[i]["body"][0]["y"] == head_y - 2):
+				if(len(other_snakes[i]["body"]) >= len(body)):
+					direction = directions[(directions.index(direction)+1) % len(directions)]
+		elif(direction == "down"):
+			if(other_snakes[i]["body"][0]["y"] == head_y + 2):
+				if(len(other_snakes[i]["body"]) >= len(body)):
+					direction = directions[(directions.index(direction)+1) % len(directions)]
 
 	if(direction == 'none'):
 		if(len(directions) > 0):
@@ -133,6 +185,8 @@ def move():
 		
 
 	return move_response(direction)
+
+	
 
 
 @bottle.post('/end')
